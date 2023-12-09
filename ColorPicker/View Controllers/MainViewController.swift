377d7473d177bf022170b1d1cd9a59a3cac5343f
That Guy Vic <vicsergeevrov@icloud.com>
2023-12-09 21:07:42 +0300

    UI upd
    fixed color labels stack
    fixed value labels stack

---
+++ ColorPicker/View Controllers/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  ColorPicker
+//
+//  Created by Vic on 09.12.2023.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
